7de28db63d99c34cf0ec8ee05b598dddf856fd46
ktoto <Ktotorus@gmail.com>
2024-06-24 22:14:28 +0500

    Промежуточный

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import os
 import aiohttp
 import aiosqlite
 import asyncio
+import csv
 import argparse
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
@@ -73,6 +73,7 @@ async def create_database_and_tables(db_file, semaphore):
             CREATE TABLE IF NOT EXISTS offers (
                 id INTEGER PRIMARY KEY,
                 product_id INTEGER,
+                size TEXT,
                 retail_price REAL,
                 discount_price REAL,
                 discount BOOLEAN,
@@ -127,87 +128,121 @@ async def insert_subcategory_to_db(db_file, subcategory, top_category_id):
 
         await conn.commit()
 
-        # Рекурсивная функция для обработки вложенных подкатегорий
         if 'subcategories' in subcategory:
             for subsub in subcategory['subcategories']:
                 await insert_subcategory_to_db(db_file, subsub, top_category_id)
 
 
-async def insert_product_to_db(db_file, product, subcategory_id, semaphore):
-    async with aiosqlite.connect(db_file, timeout=120) as conn:
-        cursor = await conn.cursor()
+async def insert_product_to_db(db_file, product, subcategory_id, semaphore, save_to_csv, csv_writer):
+    if save_to_csv is not True:
+        async with aiosqlite.connect(db_file, timeout=120) as conn:
+            cursor = await conn.cursor()
+            await cursor.execute('''
+                INSERT OR REPLACE INTO products (id, name, brand, subcategory_id)
+                VALUES (?, ?, ?, ?)
+            ''', (product['id'], product['name'], product['brand'], subcategory_id))
 
-        await cursor.execute('''
-            INSERT OR REPLACE INTO products (id, name, brand, subcategory_id)
-            VALUES (?, ?, ?, ?)
-        ''', (product['id'], product['name'], product['brand'], subcategory_id))
+            await conn.commit()
 
-        await conn.commit()
-
-        tasks = [insert_offer_to_db(db_file, offer['id'], product['id'], semaphore) for offer in product['offers']]
-        await asyncio.gather(*tasks)
+    tasks = [insert_offer_to_db(db_file, offer['id'], product['id'], product['name'], semaphore, save_to_csv, csv_writer) for offer
+             in product['offers']]
+    await asyncio.gather(*tasks)
 
 
-async def insert_offer_to_db(db_file, offer_id, product_id, semaphore):
+async def insert_offer_to_db(db_file, offer_id, product_id, product_name, semaphore, save_to_csv, csv_writer):
     async with aiohttp.ClientSession() as session:
         offer_details = await fetch_offer_details(session, offer_id, semaphore)
 
     if not offer_details:
         return
 
-    async with aiosqlite.connect(db_file, timeout=120) as conn:
-        cursor = await conn.cursor()
-
-        discount = float(offer_details['discount_price']) < float(offer_details['retail_price'])
-        await cursor.execute('''
-            INSERT OR REPLACE INTO offers (id, product_id, retail_price, discount_price, discount, vendor_code, is_available)
-            VALUES (?, ?, ?, ?, ?, ?, ?)
-        ''', (offer_details['id'], product_id, offer_details['retail_price'], offer_details['discount_price'], discount,
-              offer_details['vendor_code'], offer_details['availability_info']['is_available']))
-
+    if save_to_csv:
         for shop in offer_details['availability_info']['offer_store_amount']:
+            csv_writer.writerow({
+                'city': shop['address'].split(',')[0],
+                'address': shop['address'],
+                'vendor_code': offer_details['vendor_code'],
+                'product_name': product_name,
+                'size': offer_details['size'],
+                'retail_price': offer_details['retail_price'],
+                'discount_price': offer_details['discount_price'],
+                'availability_text': shop['availability']['text']
+            })
+    else:
+        async with aiosqlite.connect(db_file, timeout=120) as conn:
+            cursor = await conn.cursor()
+
+            discount = float(offer_details['discount_price']) < float(offer_details['retail_price'])
             await cursor.execute('''
-                INSERT OR REPLACE INTO shops (id, address, subways)
-                VALUES (?, ?, ?)
-            ''', (shop['shop_id'], shop['address'], ', '.join([subway['name'] for subway in shop['subways']])))
+                INSERT OR REPLACE INTO offers (id, product_id, size, retail_price, discount_price, discount, vendor_code, is_available)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            ''', (offer_details['id'], product_id, offer_details['size'], offer_details['retail_price'], offer_details['discount_price'], discount,
+                  offer_details['vendor_code'], offer_details['availability_info']['is_available']))
 
-            await cursor.execute('''
-                INSERT OR REPLACE INTO offer_shop_amount (offer_id, shop_id, availability_text)
-                VALUES (?, ?, ?)
-            ''', (offer_details['id'], shop['shop_id'], shop['availability']['text']))
+            for shop in offer_details['availability_info']['offer_store_amount']:
+                await cursor.execute('''
+                    INSERT OR REPLACE INTO shops (id, address, subways)
+                    VALUES (?, ?, ?)
+                ''', (shop['shop_id'], shop['address'], ', '.join([subway['name'] for subway in shop['subways']])))
 
-        await conn.commit()
+                await cursor.execute('''
+                    INSERT OR REPLACE INTO offer_shop_amount (offer_id, shop_id, availability_text)
+                    VALUES (?, ?, ?)
+                ''', (offer_details['id'], shop['shop_id'], shop['availability']['text']))
+
+            await conn.commit()
 
 
-async def process_subcategories_and_products(session, db_file, subcategories, top_category_id, semaphore):
+async def process_subcategories_and_products(session, db_file, subcategories, top_category_id, semaphore, save_to_csv,
+                                             csv_writer):
+
     for subcategory in subcategories:
-        await insert_subcategory_to_db(db_file, subcategory, top_category_id)
+        if save_to_csv is not True:
+            await insert_subcategory_to_db(db_file, subcategory, top_category_id)
 
         products_data = await fetch_products_data(session, subcategory['id'])
-        tasks = [insert_product_to_db(db_file, product, subcategory['id'], semaphore) for product in products_data]
+        tasks = [insert_product_to_db(db_file, product, subcategory['id'], semaphore, save_to_csv, csv_writer) for
+                 product in products_data]
         await asyncio.gather(*tasks)
 
         if 'subcategories' in subcategory:
             await process_subcategories_and_products(session, db_file, subcategory['subcategories'], top_category_id,
-                                                     semaphore)
+                                                     semaphore, save_to_csv, csv_writer)
 
 
-async def main(db_file, max_requests):
+async def main(db_file, max_requests, save_to_csv):
     categories_api_url = f'{URL}/api/local/v1/catalog/categories'
+    csv_file = 'products.csv'
 
     semaphore = asyncio.Semaphore(max_requests)
 
     async with aiohttp.ClientSession() as session:
         categories_data = await fetch_categories_data(session, categories_api_url)
 
-        await create_database_and_tables(db_file, semaphore)
+        if not save_to_csv:
+            await create_database_and_tables(db_file, semaphore)
 
-        for category in categories_data:
-            print(f"Сбор категории: {category['name']}")
-            await insert_category_to_db(db_file, category, semaphore)
-            if 'subcategories' in category:
-                await process_subcategories_and_products(session, db_file, category['subcategories'], category['id'],
-                                                         semaphore)
+        if save_to_csv:
+            with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
+                fieldnames = ['city', 'address', 'vendor_code', 'product_name', 'size', 'retail_price', 'discount_price',
+                              'availability_text']
+                csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
+                csv_writer.writeheader()
+
+                for category in categories_data:
+                    print(f"Сбор категории: {category['name']}")
+                    if not save_to_csv:
+                        await insert_category_to_db(db_file, category, semaphore)
+                    if 'subcategories' in category:
+                        await process_subcategories_and_products(session, db_file, category['subcategories'],
+                                                                 category['id'], semaphore, save_to_csv, csv_writer)
+        else:
+            for category in categories_data:
+                print(f"Сбор категории: {category['name']}")
+                await insert_category_to_db(db_file, category, semaphore)
+                if 'subcategories' in category:
+                    await process_subcategories_and_products(session, db_file, category['subcategories'],
+                                                             category['id'], semaphore, save_to_csv, None)
 
     print("Data insertion completed.")
 
@@ -216,12 +251,15 @@ if __name__ == "__main__":
     try:
         URL = os.getenv('URL')
         parser = argparse.ArgumentParser(description='Асинхронный скрипт для извлечения и хранения данных из API')
-        parser.add_argument('--db', type=str, default='products.db', help='Имя файла базы данных SQLite')
+        parser.add_argument('--db', type=str, help='Имя файла базы данных SQLite для сохранения данных')
         parser.add_argument('-rl', '--request-limit', type=int, default=10,
                             help='Максимальное ограничение на одновременные запросы')
         args = parser.parse_args()
 
-        asyncio.run(main(args.db, args.request_limit))
+        save_to_csv = not bool(args.db)
+
+        asyncio.run(main(args.db if args.db else 'products.db', args.request_limit, save_to_csv))
 
     except KeyboardInterrupt:
         print("\nВыход по команде Ctrl+C. Завершение программы...")
+        exit()
